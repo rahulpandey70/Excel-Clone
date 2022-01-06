b272bf25e0b7f99d89a933f60b37944eb3f1fecb
rahulpandey70 <rahulpandey9103@gmail.com>
2022-01-06 14:33:03 +0530

    Modified html file

--- js/cutCopyPaste.js
+++ js/cutCopyPaste.js
@@ -46,3 +46,50 @@ function handleSelectedCellsUI() {
     cell.style.border = "1px solid lightgrey";
   }
 }
+
+// Copy
+let copyData = [];
+copyBtn.addEventListener("click", (e) => {
+  for (let i = rangeStorage[0][0]; i <= rangeStorage[1][0]; i++) {
+    let copyRow = [];
+    for (let j = rangeStorage[0][1]; j <= rangeStorage[1][1]; j++) {
+      let cellProp = sheetDB[i][j];
+      copyRow.push(cellProp);
+    }
+    copyData.push(copyRow);
+  }
+  handleSelectedCellsUI();
+});
+
+// Paste copied data
+pasteBtn.addEventListener("click", (e) => {
+  if (rangeStorage.length < 2) return;
+
+  let rowDiff = Math.abs(rangeStorage[0][0] - rangeStorage[1][0]);
+  let colDiff = Math.abs(rangeStorage[0][1] - rangeStorage[1][1]);
+
+  let address = addressBar.value;
+  let [stRow, stCol] = decodeRICIDFromAddress(address);
+
+  for (let i = stRow, r = 0; i <= stRow + rowDiff; i++, r++) {
+    for (let j = stCol, c = 0; j <= stCol + colDiff; j++, c++) {
+      let cell = document.querySelector(`.cell-container[rid="${i}"][cid="${j}"]`);
+      if (!cell) continue;
+
+      let data = copyData[r][c];
+      let cellProp = sheetDB[i][j];
+
+      cellProp.value = data.value;
+      cellProp.bold = data.bold;
+      cellProp.italic = data.italic;
+      cellProp.underline = data.underline;
+      cellProp.fontSize = data.fontSize;
+      cellProp.fontFamily = data.fontFamily;
+      cellProp.fontColor = data.fontColor;
+      cellProp.BGcolor = data.BGcolor;
+      cellProp.alignment = data.alignment;
+
+      cell.click();
+    }
+  }
+});
